4616cb8b0fce173f2ab2f56ff5e6d36216b77ad3
AlexMedinaUma <80314351+AlexMedinaUma@users.noreply.github.com>
2022-05-23 23:08:14 +0200

    Commit4

---
+++ planetas/planetas.js
@@ -0,0 +1,15 @@
+//Navbar
+const menu = document.querySelector(".menu");
+const planetList = document.querySelector(".planets");
+//Hamburger
+menu.addEventListener("click", (event) => {
+  planetList.classList.toggle("hide-mobile");
+  menu.classList.toggle("opacity");
+});
+//Close menu only on mobile
+planetList.addEventListener("click", (event) => {
+  const screenSize = document.documentElement.clientWidth || window.innerWidth;
+  if (screenSize <= 835) {
+    planetList.classList.toggle("hide-mobile");
+  }
+});
